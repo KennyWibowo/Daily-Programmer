11326433c170622af00597367411f8551eeeff44
Kenny Wibowo <kewibowo@ucsd.edu>
2015-08-18 16:59:27 -0700

    Added old solution from #223

---
+++ #223_2015-07-15/fortune.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main( int argc, char **argv ) {
+    
+    if ( argc != 3 ) {
+        printf( "Usage: %s [Secret word] [Test problem word]\n", argv[0] );
+        return;
+    }
+
+    testWords( argv[1], argv[2] );
+
+}
+
+int testWords( char *secret_word, char *test_word ) {
+    int test_pos = 0;
+    int secret_pos = 0;
+    
+    char *test_builder = malloc( sizeof(*secret_word) );
+
+    //TODO: free test_builder, for loop to actually compare
+}
