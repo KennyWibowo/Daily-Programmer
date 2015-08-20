7dc34b1a5a8f4a2524aa3f1a41a9037538c9b767
Kenny Wibowo <kewibowo@ucsd.edu>
2015-08-19 23:03:41 -0700

    refactored paths to add clarity

--- #228_2015-08-17/alpha.js
+++
@@ -1,8 +0,0 @@
-if(process.argv.length < 3){
-	console.log('Expected one argument');
-} else {
-	var word = process.argv[2];
-	for(var i=0; i<word.length; i++) {
-		console.log(word.charAt(i));
-	}
-}
