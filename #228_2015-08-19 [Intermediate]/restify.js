dd4308dfcefc9b3f26ecda664c9f7418b7e15726
Kenny Wibowo <kewibowo@ucsd.edu>
2015-08-19 23:35:55 -0700

    reformatting tabs to spaces, and added comments

--- #228_2015-08-19 [Intermediate]/restify.js
+++ #228_2015-08-19 [Intermediate]/restify.js
@@ -2,63 +2,86 @@ var Client = require('node-rest-client').Client;
 
 var rest_client = new Client();
 
-rest_client.get("http://api.bitcoincharts.com/v1/trades.csv?symbol=rockUSD", function(data, response) {
-	var parsed_payload = parsepayload(String(data));
+// sends a GET to the rest API
+rest_client.get("http://api.bitcoincharts.com/v1/trades.csv?symbol=rockUSD", 
+    function(data, response) {
+        // parse the resultant payload
+        var parsed_payload = parsePayload(String(data));
 
-	var newest_time = parsed_payload[0][0];
-	var newest_time_pos = 0;
+        // Finds the latest data line from the server (probably not needed)
+        var newest_time = parsed_payload[0][0];
+        var newest_time_pos = 0;
+        for(var i = 1; i < parsed_payload.length; ++i) {
+            if(newest_time < parsed_payload[i][0]) {
+                newest_time = parsed_payload[i][0];
+                newest_time_pos = i;
+            }
+        }
 
-	for(var i = 1; i < parsed_payload.length; ++i) {
-		if(newest_time < parsed_payload[i][0]) {
-			newest_time = parsed_payload[i][0];
-			newest_time_pos = i;
-		}
-	}
+        // Output the latest bitcoin-to-USD price
+        console.log("Current bitcoin valued at: $" 
+            + Math.round(parsed_payload[newest_time_pos][1]*100)/100);
+              // Format float to two decimal places
+    }
+);
 
-	console.log("Current bitcoin valued at: $" +  Math.round(parsed_payload[newest_time_pos][1]*100)/100);
-})
 
-function parsepayload(payload) {
-	var data = [];
 
-	while(payload.length != 0) {
-		var iter = 0, currchar = payload.charAt(iter);
+//=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-
+//                           Function definitions
+//=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-
 
-		while((payload.length != iter) && (currchar != '\n') ){
-			currchar = payload.charAt(++iter);
-		}
-			
-		data.push(parsepayloadline(payload.slice(0, iter)))
-		payload = replaceRange(payload, 0, iter);
+// Parameter: Takes in String formatted "a,b,c,d,..." of n number of lines
+// Returns: The data parsed into a double array
+function parsePayload(payload) {
+    var data = [];
 
-		if( currchar == '\n' )
-			payload = replaceRange(payload, 0, 1); //get rid of new line
+    // Iterate through the payload
+    while(payload.length != 0) {
+        var iter = 0, currchar = payload.charAt(iter);
 
-	}
-	
-	return data;
+        // Position iter after each line
+        while((payload.length != iter) && (currchar != '\n') )
+            currchar = payload.charAt(++iter);
+            
+        // slice and parse the line, and add resultant array to data
+        data.push(parsePayloadLine(payload.slice(0, iter)));
+        payload = removeRange(payload, 0, iter); // remove the extracted line
+
+        if( currchar == '\n' )
+            payload = removeRange(payload, 0, 1); //get rid of new line
+
+    }
+    
+    return data;
 }
 
-function parsepayloadline(line) {
-	var linedata = [];
+// Parameter: Takes in format n,n,n,n,...,n
+// Returns: The data parsed into an array
+function parsePayloadLine(line) {
+    var linedata = [];
 
-	while(line.length != 0){
-		var iter = 0, currchar = line.charAt(iter);
+    // Iterate through line
+    while(line.length != 0){
+        var iter = 0, currchar = line.charAt(iter);
 
-		while((currchar != ',') && (iter != line.length))
-			currchar = line.charAt(++iter);
+        // Position iter after each element
+        while((currchar != ',') && (iter != line.length))
+            currchar = line.charAt(++iter);
 
-		linedata.push(line.slice(0, iter))
-		line = replaceRange(line, 0, iter);
+        // slice the element and add resultant element to linedata
+        linedata.push(line.slice(0, iter);
+        line = removeRange(line, 0, iter); // remove the extracted element
 
-		if(currchar == ',')
-			line  = replaceRange(line, 0,1) // get rid of comma
+        if(currchar == ',')
+            line  = removeRange(line, 0,1) // get rid of comma
 
-	}
+    }
 
-	return linedata;
+    return linedata;
 }
 
-function replaceRange(s, start, end) {
-    return s.substring(0, start) + s.substring(end);
+// Removes a substring of specified range and returns result
+function removeRange(str, start, end) {
+    return str.substring(0, start) + str.substring(end);
 }
