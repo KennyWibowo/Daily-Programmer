2ff45bdd22dfee286dd63d8e50d66884627a80f9
Kenny Wibowo <kewibowo@ucsd.edu>
2015-08-21 20:11:41 -0700

    tabs -> spaces

--- #228_2015-08-17 [Easy]/alpha.js
+++ #228_2015-08-17 [Easy]/alpha.js
@@ -1,31 +1,31 @@
 if(process.argv.length < 3){
-	console.log('Expected one argument');
+    console.log('Expected one argument');
 } else {
-	var word = process.argv[2];
-	
-	var prev = word.charCodeAt(0);
-	var curr = word.charCodeAt(1);
+    var word = process.argv[2];
+    
+    var prev = word.charCodeAt(0);
+    var curr = word.charCodeAt(1);
 
-	var direction = prev < curr ? "forward" : "backward";
+    var direction = prev < curr ? "forward" : "backward";
 
-	prev = curr;
+    prev = curr;
 
-	for(var i=2; i<word.length; i++) {
-		var curr = word.charCodeAt(i);
+    for(var i=2; i<word.length; i++) {
+        var curr = word.charCodeAt(i);
 
-		var thisDir = prev < curr ? "forward" : "backward";
+        var thisDir = prev < curr ? "forward" : "backward";
 
-		if(thisDir != direction) {
-			direction = "oh noes!";
-			break;
-		}
-	}
+        if(thisDir != direction) {
+            direction = "oh noes!";
+            break;
+        }
+    }
 
-	var message = (direction == "forward" || direction == "oh noes!") ? "IN ORDER" : "REVERSE ORDER"
+    var message = (direction == "forward" || direction == "oh noes!") ? "IN ORDER" : "REVERSE ORDER"
 
-	if(direction == "oh noes!") {
-		message = "NOT ".concat(message);
-	}
+    if(direction == "oh noes!") {
+        message = "NOT ".concat(message);
+    }
 
-	console.log(message);
+    console.log(message);
 }
