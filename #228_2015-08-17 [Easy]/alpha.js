49fcd229b6f5959010434fbf7fcf13c09c91c387
Kenny Wibowo <kewibowo@ucsd.edu>
2015-08-21 20:09:25 -0700

    Finished up JS implementation of #228: Easy

--- #228_2015-08-17 [Easy]/alpha.js
+++ #228_2015-08-17 [Easy]/alpha.js
@@ -2,7 +2,30 @@ if(process.argv.length < 3){
 	console.log('Expected one argument');
 } else {
 	var word = process.argv[2];
-	for(var i=0; i<word.length; i++) {
-		console.log(word.charAt(i));
+	
+	var prev = word.charCodeAt(0);
+	var curr = word.charCodeAt(1);
+
+	var direction = prev < curr ? "forward" : "backward";
+
+	prev = curr;
+
+	for(var i=2; i<word.length; i++) {
+		var curr = word.charCodeAt(i);
+
+		var thisDir = prev < curr ? "forward" : "backward";
+
+		if(thisDir != direction) {
+			direction = "oh noes!";
+			break;
+		}
 	}
+
+	var message = (direction == "forward" || direction == "oh noes!") ? "IN ORDER" : "REVERSE ORDER"
+
+	if(direction == "oh noes!") {
+		message = "NOT ".concat(message);
+	}
+
+	console.log(message);
 }
