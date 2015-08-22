2493ef8a74721ce765711b636c6f057b66fa6cc2
Kenny Wibowo <kewibowo@ucsd.edu>
2015-08-21 21:38:46 -0700

    added comments

--- #228_2015-08-17 [Easy]/alpha.js
+++ #228_2015-08-17 [Easy]/alpha.js
@@ -1,21 +1,30 @@
 if(process.argv.length < 3){
     console.log('Expected one argument');
 } else {
+    // Take in parameter
     var word = process.argv[2];
 
     console.log(testWord(word));
 }
 
+//=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-
+//                           Function definitions
+//=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-
+
+// Parameter: Takes in any single word
+// Returns: Message determining if word is "IN ORDER", "NOT IN ORDER", or "REVERSE ORDER"
 function testWord(word) {
 
+    // One character test
     if(word.length == 1) {
         return "IN ORDER";
     }
 
+    // Test first two characters to determine direction
     var prev = word.charCodeAt(0);
     var curr = word.charCodeAt(1);
 
-    var direction = prev < curr ? "forward" : "backward";
+    var direction = prev < curr ? "forward" : "backward"; // Note: but what if it's the same letter?
 
     prev = curr;
 
@@ -24,7 +33,8 @@ function testWord(word) {
 
         var thisDir = prev < curr ? "forward" : "backward";
 
-        if(thisDir != direction) {
+        // if it's not in order, oh well
+        if(curr != prev && thisDir != direction) {
             direction = "oh noes!";
             break;
         }
@@ -32,11 +42,11 @@ function testWord(word) {
         prev = curr;
     }
 
+    // This line's just for fun
     var message = (direction == "forward" || direction == "oh noes!") ? "IN ORDER" : "REVERSE ORDER"
 
-    if(direction == "oh noes!") {
+    if(direction == "oh noes!")
         message = "NOT ".concat(message);
-    }
 
     return message;
 }
