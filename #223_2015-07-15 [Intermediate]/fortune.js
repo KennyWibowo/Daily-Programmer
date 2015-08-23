be003247704f2de9342151680e25b34085986a0b
Kenny Wibowo <kewibowo@ucsd.edu>
2015-08-23 00:20:25 -0700

    added more supporting functions!

--- #223_2015-07-15 [Intermediate]/fortune.js
+++ #223_2015-07-15 [Intermediate]/fortune.js
@@ -10,24 +10,55 @@ if(process.argv.length < 4){
 
 
 function queryOffense(word_puzzle, word_offense) {
-	var iter_query = 0;
+    var chars_offense = getUniqueChars(word_offense);
+    var str_builder = [];
 
-	for(var i =0; i < word_offense.length; i++) {
+    for(var i=0; i < word_puzzle.length; i++) {
+        str_builder.push(null);
+    }
 
-			iter_query++;
-	}
+    for(var i=0; i < chars_offense.length; i++)
+        for(var j=0; j < word_puzzle.length; j++)
+            if(word_puzzle.charAt(j) == chars_offense[i])
+                str_builder[j] = word_puzzle.charAt(j);
 
-	if(iter_query== word_offense.length)
-		return true;
-
-	return false;
-}
+    console.log(arrToString(str_builder));
+}   
 
 function getUniqueChars(word) {
-	var chars = [];
-	for(var i=0 ; i<word.length; i++) 
-		if(!chars.includes(word.charAt(i)))
-			chars.push(word.charAt(i));
+    var chars = [];
+    for(var i=0 ; i<word.length; i++) 
+        if(!chars.indexOf(word.charAt(i)))
+            chars.push(word.charAt(i));
 
-	return chars;
+    return chars;
+}
+
+function testContains(word, query) {
+    var iter_query = 0;
+
+    for(var i =0; i < word.length; i++)
+        if(word.charAt(i) == query.charAt(iter_query))
+            iter_query++;
+    
+    if(iter_query== query.length)
+        return true;
+
+    return false;
+}
+
+function arrToString(arr) {
+    var str = "";
+    for(var i=0; i< arr.length;i++)
+        if(arr[i])
+            str += arr[i];
+
+    return str;
+}
+
+function arrayIncludes(arr, item){
+    for(var i =0; i< arr.length; i++)
+        if(arr[i] == item)
+            return true;
+    return false;
 }
