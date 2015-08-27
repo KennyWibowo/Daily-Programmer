cbdc3f44f10b82a29f8fa04da21d657c440f50d1
Kenny Wibowo <kewibowo@ucsd.edu>
2015-08-26 20:19:37 -0700

    parses order of characters

--- #229_2015-08-26 [Intermediate]/buzzfizz.js
+++ #229_2015-08-26 [Intermediate]/buzzfizz.js
@@ -8,25 +8,42 @@ var fs = require('fs'), filename = process.argv[2];
 fs.readFile(filename, 'utf8', function(err, filedata) {
     if (err) throw err;
 
-    parseFileData(filedata, function(data) {
-        console.log(data);
-    });
+    parseFileData(filedata, buzzfizz);
 });
 
 function parseFileData(filedata, next) {
-    var data = [];
-    var pos = 0;
+    var data = [],
+        dataorder = {},
+        pos = 0;
+
     for(var i=0; i< filedata.length; i++) {
         var currchar = filedata.charAt(i);
-        if(currchar == "\n") {
-            data.push(filedata.substring(pos, i));
+        if(currchar == "\n") { 
+            var line = filedata.substring(pos, i);
+            dataorder = parseFileDataLine(line, dataorder);
+            data.push(line);
             pos = i+1;
         } else if (currchar == "\r") {
-            data.push(filedata.substring(pos, i));
+            var line = filedata.substring(pos, i);
+            dataorder = parseFileDataLine(line, dataorder);
+            data.push(line);
             pos = i+2;
             i++;
         }
     }
 
-    next(data);
+    next(data, dataorder);
+}
+
+function buzzfizz(data, dataorder) {
+    console.log(data);
+    console.log(dataorder); 
+}
+
+function parseFileDataLine(line, dataorder) {
+    for(var j = 0; j < line.length; j++)
+        if(!dataorder[line.charAt(j)])
+            dataorder[line.charAt(j)] = Object.keys(dataorder).length + 1;
+
+    return dataorder;
 }
