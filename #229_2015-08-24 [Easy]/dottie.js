dcc09da1495522bef44b7319066cb100ed365924
Kenny Wibowo <kewibowo@ucsd.edu>
2015-08-26 19:40:35 -0700

    added iteration count and improved accuracy

--- #229_2015-08-24 [Easy]/dottie.js
+++ #229_2015-08-24 [Easy]/dottie.js
@@ -4,17 +4,20 @@ if(process.argv.length < 3){
     // Take in parameter
     var number = process.argv[2];
 
-    console.log(dottify(number));
+    console.log("Result: " + dottify(number));
 }
 
 function dottify(number) {
 	var abs = 0.739085; // The real dottie number
-	var diff = 0.000005; // + or - this number
+	var diff = 0.0000001; // + or - this number
 
+	var iter = 0;
 
 	while(Math.abs(number - abs) > diff) {
 		number = Math.cos(number);
+		iter ++;
 	}
 
+	console.log("Iterated " + iter + " times");
 	return number;
 }
