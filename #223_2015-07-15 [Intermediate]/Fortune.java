1239f37cb78521848a21d94244e24ffbe089f039
Kenny Wibowo <kewibowo@ucsd.edu>
2015-08-30 22:06:16 -0700

    Created function to hold unique chars from offensive word #223

--- #223_2015-07-15 [Intermediate]/Fortune.java
+++ #223_2015-07-15 [Intermediate]/Fortune.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Fortune {
 	public static void main( String args[] ){
 		if(args.length < 2) {
@@ -7,5 +9,20 @@ public class Fortune {
 
 		String word_puzzle = args[0];
 		String word_offense = args[1];
+
+		HashMap<Character, Integer> letters = parseWord( word_offense );
+
+		char stringbuilder[] = new char[word_puzzle.length()];
+
+	}
+
+	private static HashMap<Character, Integer> parseWord ( String word ) {
+		HashMap<Character, Integer> letters = new HashMap<Character, Integer>();
+
+		for(int i=0; i<word.length(); i++) {
+			letters.put(word.charAt(i), 0);
+		}
+
+		return letters;
 	}
 }
