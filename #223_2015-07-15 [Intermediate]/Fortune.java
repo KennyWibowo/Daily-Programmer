b7551c65f9580769348b5487851ff05c6a56edf7
Kenny Wibowo <kewibowo@ucsd.edu>
2015-08-30 19:10:35 -0700

    Started Java implementation of #223

---
+++ #223_2015-07-15 [Intermediate]/Fortune.java
@@ -0,0 +1,11 @@
+public class Fortune {
+	public static void main( String args[] ){
+		if(args.length < 2) {
+			System.out.println("Please specify two inputs.");
+			System.exit(-1);
+		}
+
+		String word_puzzle = args[0];
+		String word_offense = args[1];
+	}
+}
