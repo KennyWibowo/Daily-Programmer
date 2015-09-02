27b868ec6deeec278fd34c4bc604c5e08e57557b
Kenny Wibowo <kewibowo@ucsd.edu>
2015-09-01 22:29:51 -0700

    Implement object and parser

--- #230_2015-08-31 [Easy]/TreasureHunt.java
+++ #230_2015-08-31 [Easy]/TreasureHunt.java
@@ -8,12 +8,13 @@ public class TreasureHunt {
 			System.exit(-1);
 		}
 
-		List<String> input;
+		String input;
+		String query = "dailyprogrammer";
 
 		try {
 			
 			input = readFile(args[0]);
-			System.out.println(input.size());
+			System.out.println(parseJSON(input, query));
 
 		} catch (Exception e) {
 			System.out.println("Error: " + e.getMessage());
@@ -21,21 +22,76 @@ public class TreasureHunt {
 
 	}
 
-	private static List<String> readFile(String filename) throws Exception {
-	    String line = null;
-	    List<String> records = new ArrayList<String>();
-	 
-	    // wrap a BufferedReader around FileReader
+	private static String readFile(String filename) throws Exception {
 	    BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
-	 
-	    // use the readLine method of the BufferedReader to read one line at a time.
-	    // the readLine method returns null when there is nothing else to read.
-	    while ((line = bufferedReader.readLine()) != null) {
-	        records.add(line);
-	    }
-	   
+	    String line = bufferedReader.readLine();
+
 	    // close the BufferedReader when we're done
 	    bufferedReader.close();
-	    return records;
+	    
+	    return line;
 	}
+	
+	private static String parseJSON(String json, String query) {
+		boolean found = false;
+		int pos = -1;
+		String dir = "";
+
+		for( int j=0; j<json.length(); j++ ) {
+			if( json.charAt(j) == query.charAt(0) ) {
+				j++;
+
+				for( int q=1; q<query.length() && j<json.length(); q++, j++ ) {
+					if( json.charAt(j) != query.charAt(q) ) {
+						break;
+					} else if( q == query.length()-1 ) {
+						found = true;
+						pos = j- (query.length()-1);
+					}
+				}
+			}
+
+			if(found) {
+				break;
+			}
+		}
+
+		if(!found) {
+			return "Could not find " + query + " in JSON.";
+		}
+
+		return "Found: " + query + " at position " + pos + " (test: "+ json.substring(pos, pos + query.length())+ " )";
+	}
+}
+
+class Closures {
+	private char[] closures;
+	private boolean closed;
+	private boolean directionLeft;
+
+	public Closures(char start, char end, String direction) {
+		this.closures = new char[2];
+		this.closures[0] = start;
+		this.closures[1] = end;
+		this.closed = false;
+
+		this.directionLeft = (direction == "left");
+	}
+
+	public void closed() {
+		closed = true;
+	}
+
+	public boolean getClosed() {
+		return closed;
+	}
+
+	public String getDirection() {
+		if(directionLeft) {
+			return "left";
+		}
+
+		return "right";
+	}
+
 }
